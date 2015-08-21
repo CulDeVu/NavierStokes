3bcb1062ee9c491596e5c4aae45f31b39d5c41c6
Daniel Taylor <dannyjtaylor1@gmail.com>
2015-08-21 17:06:53 -0400

    advection
    3 different methods for advection were finished

--- NavierStokes/main.cpp
+++ NavierStokes/main.cpp
@@ -1,4 +1,5 @@
 
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <stdlib.h>
@@ -21,18 +22,16 @@ const int imageWidth = 256,
 
 float PI = 3.14159;
 
-struct cell
-{
-	float amount;
-	vec2 vel;
-};
-
 const int cellW = 100,
 	cellH = 100;
-float density[cellW][cellH];
-float density_next[cellW][cellH];
+const float dt = 1.f / 60.f;
+
+float ink[cellW][cellH];
+float ink_next[cellW][cellH];
+
 vec2 vel[cellW][cellH];
 vec2 vel_next[cellW][cellH];
+
 float pressure[cellW][cellH];
 GLFWwindow* window;
 
@@ -56,7 +55,7 @@ void setupParticles()
 	{
 		for (int y = 0; y < cellH; ++y)
 		{
-			//density[x][y] = 4 * (float)y / cellH;
+			//ink[x][y] = 4 * (float)y / cellH;
 
 			vec2 v1 = vec2(x - 30, y - 30) / 10.f;
 			vec2 v2 = vec2(x - 70, y - 70) / 5.f;
@@ -69,14 +68,14 @@ void setupParticles()
 			/*vel[x][y] = vec2(
 				v1.y - (v1.x*v1.x*v1.x + v1.x),
 				-v1.x);*/
-			//vel[x][y] = vec2(-v1.y, v1.x);
+			vel[x][y] = vec2(-v1.y, v1.x);
 			//vel[x][y] = vec2();
+			//vel[x][y] = vec2(-y, x) / 10.f;
 		}
 	}
-}
 
-void updateDensityField(float cur[cellW][cellH], float next[cellW][cellH], vec2 vel[cellW][cellH], vec2 vel_next[cellW][cellH])
-{
+	for (int i = 0; i < cellH; i += 4)
+		ink[50][i] = 100;
 }
 
 template <typename T>
@@ -92,125 +91,99 @@ void copy(T src[cellW][cellH], T dst[cellW][cellH])
 	}
 }
 
-void advect()
+float clampAbove(float x)
 {
-	for (int y = 0; y < cellH; ++y)
-	{
-		for (int x = 0; x < cellW; ++x)
-		{
-			vec2 advectTarget = vec2(x, y) - vel[x][y] / 60.f;
-
-			float tu = advectTarget.x - floor(advectTarget.x);
-			float tv = advectTarget.y - floor(advectTarget.y);
-			int xt = floor(advectTarget.x);
-			int yt = floor(advectTarget.y);
-
-			float ll = (1 - tu) * (1 - tv);
-			float lh = (1 - tu) * tv;
-			float hl = tu * (1 - tv);
-			float hh = tu * tv;
-
-			if (xt < 0 || xt + 1 >= cellW || yt < 0 || yt + 1 >= cellH)
-			{
-				vel_next[x][y] += vel[x][y];
-				continue;
-			}
-
-			vel_next[x][y] += ll * vel[xt][yt];
-			vel_next[x][y] += hl * vel[xt + 1][yt];
-			vel_next[x][y] += lh * vel[xt][yt + 1];
-			vel_next[x][y] += hh * vel[xt + 1][yt + 1];
-		}
-	}
+	return x;
 }
-
-void advectDensity()
+template <typename T>
+void advectEulerian(T q[cellW][cellH], T q_next[cellW][cellH])
 {
 	for (int y = 0; y < cellH; ++y)
 	{
 		for (int x = 0; x < cellW; ++x)
 		{
-			vec2 advectTarget = vec2(x, y) - vel[x][y] / 60.f;
-
-			float tu = advectTarget.x - floor(advectTarget.x);
-			float tv = advectTarget.y - floor(advectTarget.y);
-			int xt = floor(advectTarget.x);
-			int yt = floor(advectTarget.y);
-
-			float ll = (1 - tu) * (1 - tv);
-			float lh = (1 - tu) * tv;
-			float hl = tu * (1 - tv);
-			float hh = tu * tv;
-
-			if (xt < 0 || xt + 1 >= cellW || yt < 0 || yt + 1 >= cellH)
-			{
-				density_next[x][y] += density[x][y];
+			if (x - 1 < 0 || x + 1 > cellW - 1 || y < 0 || y + 1 > cellH - 1)
 				continue;
-			}
 
-			density_next[x][y] += ll * density[xt][yt];
-			density_next[x][y] += hl * density[xt + 1][yt];
-			density_next[x][y] += lh * density[xt][yt + 1];
-			density_next[x][y] += hh * density[xt + 1][yt + 1];
+			q_next[x][y] = q[x][y];
+
+			T dq_dx = (q[x + 1][y] - q[x - 1][y]) / 2.f;
+			T dq_dy = (q[x][y + 1] - q[x][y - 1]) / 2.f;
+			
+			T u_dot_del_q = vel[x][y].x * dq_dx + vel[x][y].y * dq_dy;
+
+			T dq_dt = -u_dot_del_q;
+			q_next[x][y] += dq_dt * dt;
+			//q_next[x][y] = clamp(q_next[x][y], -10.f, 10000000.f);
 		}
 	}
 }
 
 template <typename T>
-T JacobiIteration(int xpos, int ypos, T x[cellW][cellH], T b, float a, float beta)
+void advectForwardLangrangian(T q[cellW][cellH], T q_next[cellW][cellH])
 {
-	T newVal = x[xpos - 1][ypos] + x[xpos + 1][ypos] + x[xpos][ypos - 1] + x[xpos][ypos + 1] + a * b;
-	newVal = newVal / (beta);
-	return newVal;
-}
-
-void diffuse()
-{
-	float dx = 1;
-	float dt = 1 / 60.f;
-	float a = dx*dx / (dt);
-
-	for (int k = 0; k < 20; ++k)
+	for (int y = 0; y < cellH; ++y)
 	{
-		for (int y = 0; y < cellH; ++y)
+		for (int x = 0; x < cellW; ++x)
 		{
-			for (int x = 0; x < cellW; ++x)
-			{
-				if (x - 1 < 0 || x + 1 >= cellW || y - 1 < 0 || y + 1 >= cellH)
-				{
-					vel_next[x][y] = vel[x][y];
-				}
-				
-				vel_next[x][y] = JacobiIteration(x, y, vel, vel[x][y], a, 4 + a);
-			}
-		}
+			vec2 advectTarget = vec2(x, y) + vel[x][y] / 60.f;
 
-		copy(vel_next, vel);
+			float tu = advectTarget.x - floor(advectTarget.x);
+			float tv = advectTarget.y - floor(advectTarget.y);
+			int xt = floor(advectTarget.x);
+			int yt = floor(advectTarget.y);
+
+			float ll = (1 - tu) * (1 - tv);
+			float lh = (1 - tu) * tv;
+			float hl = tu * (1 - tv);
+			float hh = tu * tv;
+
+			if (xt < 0 || xt + 1 > cellW - 1 || yt < 0 || yt + 1 > cellH - 1)
+			{
+				continue;
+			}
+
+			q_next[xt][yt] += ll * q[x][y];
+			q_next[xt + 1][yt] += hl * q[x][y];
+			q_next[xt][yt + 1] += lh * q[x][y];
+			q_next[xt + 1][yt + 1] += hh * q[x][y];
+		}
 	}
 }
 
-void computePressure(float pressure[cellW][cellH])
+template <typename T>
+void advectBackwardsLangrangian(T q[cellW][cellH], T q_next[cellW][cellH])
 {
-	float pressure_next[cellW][cellH];
-	for (int k = 0; k < 20; ++k)
+	for (int y = 0; y < cellH; ++y)
 	{
-		for (int y = 0; y < cellH; ++y)
+		for (int x = 0; x < cellW; ++x)
 		{
-			for (int x = 0; x < cellW; ++x)
+			vec2 advectTarget = vec2(x, y) - vel[x][y] / 60.f;
+
+			float tu = advectTarget.x - floor(advectTarget.x);
+			float tv = advectTarget.y - floor(advectTarget.y);
+			int xt = floor(advectTarget.x);
+			int yt = floor(advectTarget.y);
+
+			float ll = (1 - tu) * (1 - tv);
+			float lh = (1 - tu) * tv;
+			float hl = tu * (1 - tv);
+			float hh = tu * tv;
+
+			if (xt < 0 || xt + 1 > cellW - 1 || yt < 0 || yt + 1 > cellH - 1)
 			{
-				if (x - 1 < 0 || x + 1 >= cellW || y - 1 < 0 || y + 1 >= cellH)
-				{
-					pressure_next[x][y] = pressure[x][y];
-				}
-				
-				float divergence_vel = 0;
-				divergence_vel = divergence(x, y, vel);
-
-				pressure_next[x][y] = max(0.f, JacobiIteration(x, y, pressure, divergence_vel, -1, 4));
+				//q_next[x][y] += q[x][y];
+				q_next[x][y] = T();
+				continue;
+				//xt = min(cellW - 2, max(xt, 0));
+				//yt = min(cellH - 2, max(yt, 0));
 			}
-		}
 
-		copy(pressure_next, pressure);
+			q_next[x][y] += ll * q[xt][yt];
+			q_next[x][y] += hl * q[xt + 1][yt];
+			q_next[x][y] += lh * q[xt][yt + 1];
+			q_next[x][y] += hh * q[xt + 1][yt + 1];
+		}
 	}
 }
 
@@ -238,48 +211,25 @@ void update()
 	{
 		for (int y = 0; y < cellH; ++y)
 		{
-			density_next[x][y] = 0;
+			ink_next[x][y] = 0;
 			vel_next[x][y] = vec2();
 
 			//vel[x][y] += F_grav / 60.f;
 		}
 	}
 
-	advect();
-	copy(vel_next, vel);
-
-	diffuse();
-
-	// projection operator
-	for (int y = 0; y < cellH; ++y) for (int x = 0; x < cellW; ++x) pressure[x][y] = 0;
-	computePressure(pressure);
-
-	for (int y = 0; y < cellH; ++y)
-	{
-		for (int x = 0; x < cellW; ++x)
-		{
-			if (x - 1 < 0 || x + 1 >= cellW || y - 1 < 0 || y + 1 >= cellH)
-			{
-				continue;
-			}
-			
-			vec2 grad_p = gradient(x, y, pressure);
-			vel[x][y] = vel[x][y] - grad_p;
-		}
-	}
+	advectForwardLangrangian(ink, ink_next);
+	//for (int x = 0; x < cellW; ++x) for (int y = 0; y < cellH; ++y) ink_next[x][y] = max(0.f, ink_next[x][y]);
+	//advectEulerian(vel, vel_next);
+	copy(vel, vel_next);
 
 	enforceBoundary();
-
-	advectDensity();
-
-	copy(vel, vel_next);
-	//updateDensityField(density, density_next, vel, vel_next);
 
 	for (int x = 0; x < cellW; ++x)
 	{
 		for (int y = 0; y < cellH; ++y)
 		{
-			density[x][y] = density_next[x][y];
+			ink[x][y] = ink_next[x][y];
 			vel[x][y] = vel_next[x][y];
 		}
 	}
@@ -293,21 +243,21 @@ void update()
 
 		printf("%i, %i \n", (int)rx, (int)ry);
 
-		if (0 <= rx && rx < cellW &&
+		/*if (0 <= rx && rx < cellW &&
 			0 <= ry && ry < cellH)
 		{
 			printf("-----------------------------\n");
-			printf("D: %f\n", density[(int)rx][(int)ry]);
+			printf("D: %f\n", ink[(int)rx][(int)ry]);
 			printf("V: %f, %f\n", vel[(int)rx][(int)ry].x, vel[(int)rx][(int)ry].y);
 			printf("P: %f\n", pressure[(int)rx][(int)ry]);
 			printf("-----------------------------\n\n");
-		}
+		}*/
 
 		if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT))
 		{
-			vec2 p = (vec2(rx, ry) - prevPos) * 100.f;
-			density[(int)rx][(int)ry] += 1;
-			vel[(int)rx][(int)ry] += p;
+			vec2 p = (vec2(rx, ry) - prevPos) * 10.f;
+			ink[(int)rx][(int)ry] += 1;
+			//vel[(int)rx][(int)ry] += p;
 		}
 
 		prevPos = vec2(rx, ry);
@@ -330,13 +280,13 @@ void draw()
 		{
 			for (int y = 0; y < cellH; ++y)
 			{
-				float f = pressure[x][y] / (max(0.f, pressure[x][y]) + 1);
-				if (density[x][y] >= 0 && density[x][y] < 1000)
+				float f = ink[x][y] / (max(0.f, ink[x][y]) + 1);
+				if (ink[x][y] >= 0)
 					glColor3f(f, f, f);
-				if (density[x][y] < 0)
+				/*if (ink[x][y] < 0)
 					glColor3f(0, 1, 0);
-				if (density[x][y] >= 1000)
-					glColor3f(1, 0, 0);
+				if (ink[x][y] >= 1000)
+					glColor3f(1, 0, 0);*/
 				glVertex2f(x, y);
 				glVertex2f(x + 1, y);
 				glVertex2f(x + 1, y + 1);
@@ -390,10 +340,28 @@ int main()
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
 	setupParticles();
+
 	// main loop
+	auto currentTime = chrono::high_resolution_clock::now();
+	float accumulator = 0;
+	int iter = 0;
 	while (!glfwWindowShouldClose(window))
 	{
-		update();
+		auto newTime = chrono::high_resolution_clock::now();
+		float frameTime = chrono::duration_cast<chrono::milliseconds>(newTime - currentTime).count();
+
+		if (frameTime >= dt && iter < 3000)
+		{
+			update();
+
+			if (2000 < iter && iter < 4000)
+				ink[65][20] += 1;
+
+			++iter;
+			//printf("iter %d\n", iter);
+
+			currentTime = newTime;
+		}
 		
 		draw();
 
